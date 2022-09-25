3bbb3124d3d3b8100bfc10a66f131053479bb38d
happine2s <happine2s@sookmyung.ac.kr>
2022-09-25 22:01:52 +0900

    [feat] 자바 기본문법-1

---
+++ javastudy/src/com/dinfree/java/part1/Operator1.java
@@ -0,0 +1,37 @@
+package com.dinfree.java.part1;
+
+public class Operator1 {
+
+	public static void main(String[] args) {
+		int num1=30;
+		int num2=14;
+		
+		int result1=num1*num2;
+		int result2=num1%num2;
+		
+		System.out.printf("result1=%d\n",result1);
+		System.out.printf("result2=%d\n",result2);
+		System.out.println("------------------");
+		
+		System.out.printf("num1++=%d\n", num1++);
+		System.out.printf("num1++=%d\n", num1);
+		
+		num2*=2;
+		System.out.printf("num2*2=%d\n", num2);
+		System.out.println("------------------");
+		
+		System.out.printf("num1++=%d\n", num1++);
+		System.out.printf("--num1=%d\n", --num1);
+		
+		
+		
+//		for(int i=1;i<=num1;i++) {
+//			System.out.printf("%d\t", i);
+//			if(i%5==0) {
+//				System.out.println();
+//			}
+//		}
+
+	}
+
+}
